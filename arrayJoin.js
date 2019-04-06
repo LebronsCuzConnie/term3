522ad80be775cf19b82e0c48e30ea82e7034f4a2
LebronsCuzConnie <43911409+LebronsCuzConnie@users.noreply.github.com>
2019-04-06 14:26:59 -0400

    Add files via upload

---
+++ arrayJoin.js
@@ -0,0 +1,60 @@
+/* joinArray.js
+ * Follow the prompts to print out 
+ * an interesting quote.
+*/
+
+let phrases = ["enough", "zebras are great!", "are", "think they can", "crazy", "umbrellas"];
+
+/* Sort the phrases array 
+ * in alphabetical order
+*/
+phrases.sort()
+
+
+/* Remove the last element in the
+ * phrases array
+*/
+phrases.pop()
+
+
+/* Add the string "the ones who do."
+ * to the end of the phrases array
+*/
+phrases.push("the ones who do")
+
+/* Add the string "People who" at 
+ * index 0 of the phrases array
+*/
+phrases.splice(0,0,"People who")
+
+/* Add the string "to" at 
+ * index 4 of the phrases array
+*/
+phrases.splice(4,0,"to")
+
+
+/* Replace the string "umbrellas"
+ * with the string at index 1 of the
+ * phrases array.
+ * Hint: Print your array to figure out
+ * the current index of "umbrella".
+*/
+phrases[6] = phrases[1]
+
+
+
+/* Add the phrase "change the world," 
+ * at the second to last index of the array.
+*/
+phrases.splice(6,0,"change the world")
+
+
+/* This code should print: 
+ * People who are crazy enough to 
+ * think they can change the world, 
+ * are the ones who do. 
+*/
+console.log(phrases)
+
+let quote = phrases.join(" ");
+console.log(quote);
---
+++ for.js
@@ -0,0 +1,121 @@
+/* 
+ * for.js
+ * for loop practice with inventors array
+*/
+
+let inventors = [
+	{
+		name: "Mark E. Dean",
+		gender: "Male",
+		invention: "ISA bus, color monitor, gigahertz chip",
+		liked: false
+	},
+	{
+		name: "Charles Richard Drew",
+		gender: "Male",
+		invention: "Blood banks",
+		liked: false
+	},
+	{
+		name: "Madam C.J. Walker",
+		gender: "Female",
+		invention: "African American hair care and cosmetics",
+		liked: false
+	},
+	{
+		name: "Guillermo Gonzalez Camarena",
+		gender: "Male",
+		invention: "Early color television system",
+		liked: false
+	},
+	{
+		name: "Ellen Ochoa",
+		gender: "Female",
+		invention: "Optical analysis systems",
+		liked: false
+	},
+	{
+		name: "Marie Van Brittan Brown",
+		gender: "Female",
+		invention: "Home security systems",
+		liked: false
+	},
+	{
+		name: "Alexander Miles",
+		gender: "Male",
+		invention: "Automatically closing elevator doors",
+		liked: false
+	},
+	{
+		name: "Dr. Patricia Bath",
+		gender: "Female",
+		invention: "Laserphaco Probe",
+		liked: false
+	}
+];
+
+
+/* 
+ * Print all of the inventors in the inventors array,
+ * list their properties
+*/
+console.log("------------------Inventors------------------");
+for (let i = 0; i < inventors.length; i++){
+	console.log(inventors[i]);
+}
+/* 
+ * Print the names of the female inventors
+*/
+console.log("---------------Female Inventors--------------");
+for(let i = 0; i < inventors.length; i++){
+	if (inventors[i].gender == "Female"){
+		console.log(inventors[i].name);
+	}
+}
+
+
+
+/* 
+ * Print the names of the male inventors
+*/
+console.log("----------------Male Inventors---------------");
+for(let i = 0; i < inventors.length; i++){
+	if (inventors[i].gender == "Male"){
+		console.log(inventors[i].name);
+	}
+}
+
+
+/*
+ * Half of the inventors have been liked on
+ * social media. Assign a value of true to the liked
+ * property of every other inventor. 
+*/
+for(let i = 0; i < inventors.length; i = i + 2){
+	inventors[i].liked = true;
+}
+
+
+
+
+
+/*
+ * Use a loop to count and print the number of liked inventors,
+ * meaning inventors whose liked property is set to true
+*/ 
+console.log("----------------Count # of liked inventors---------------");
+let count = 0;
+
+for(let i = 0; i < inventors.length; i++){
+	if (inventors[i].liked == true){
+		count++;
+	}
+}
+console.log(count)
+
+
+
+
+
+
+
